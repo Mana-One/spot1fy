b0a7d1faa8b0916b7f73cd1611ea976bd848af83
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-02-05 19:34:26 +0100

    rankings recycler view

--- app/src/main/java/com/manaois/spot1fy/MainActivity.kt
+++ app/src/main/java/com/manaois/spot1fy/MainActivity.kt
@@ -2,10 +2,16 @@ package com.manaois.spot1fy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rankings_list)
+        recyclerView.adapter = RankingsItemAdapter(this)
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/com/manaois/spot1fy/RankingsItemAdapter.kt
@@ -0,0 +1,31 @@
+package com.manaois.spot1fy
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RankingsItemAdapter(
+    private val context: Context,
+    private val dataset: List<String> = List(10) { "Hey I am an element !" }
+) : RecyclerView.Adapter<RankingsItemAdapter.RankingsItemViewHolder>() {
+
+    class RankingsItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView = view.findViewById<TextView>(R.id.rankings_item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RankingsItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.rankings_item, parent, false)
+        return RankingsItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: RankingsItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = "$position - $item"
+    }
+
+    override fun getItemCount() = dataset.size
+}

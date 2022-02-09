dd78735c9551558394ab016c9754a9574d9366f2
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-02-09 23:13:40 +0100

    redesigned search bar

--- app/src/main/java/com/manaois/spot1fy/search/SearchFragment.kt
+++ app/src/main/java/com/manaois/spot1fy/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.manaois.spot1fy.R
@@ -20,7 +21,12 @@ class SearchFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.search_list).apply {
+        val editText: EditText = view.findViewById(R.id.search_input)
+        view.findViewById<View>(R.id.search_cancel_icon).setOnClickListener {
+            editText.setText("")
+        }
+
+        view.findViewById<RecyclerView>(R.id.search_list).apply {
             adapter = SearchListItemAdapter(requireContext())
         }
 
